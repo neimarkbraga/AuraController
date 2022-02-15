0fb072a326bf598484f98074330774c6e6ca660b
Makoy <neimarkbraga@gmail.com>
2022-02-16 00:11:50 +0800

    init

---
+++ AuraController/HttpServer.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AuraController
+{
+    struct RouteHandler
+    {
+        public string method;
+        public string path;
+        public Action<HttpListenerRequest, HttpListenerResponse, Func<bool>> handler;
+    }
+
+    class HttpServer
+    {
+        private HttpListener listener;
+   
+        private List<RouteHandler> handlers = new List<RouteHandler>();
+
+        private async static void Send(byte[] data, HttpListenerResponse res)
+        {
+            res.ContentEncoding = Encoding.UTF8;
+            res.ContentLength64 = data.LongLength;
+            await res.OutputStream.WriteAsync(data, 0, data.Length);
+            res.Close();
+        }
+
+        private bool IsResponseResolved(HttpListenerResponse res)
+        {
+            try
+            {
+                res.OutputStream.GetType();
+                return false;
+            }
+            catch (ObjectDisposedException e)
+            {
+                return true;
+            }
+        }
+
+        private bool PathMatched(HttpListenerRequest req, string handlerPath)
+        {
+            string reqPath = new Regex(@"\?.*").Replace(req.RawUrl, "");
+            if (WebUtility.UrlDecode(reqPath) == handlerPath)
+                return true;
+            
+            if (new Regex(@"\/:[^\/]+").IsMatch(handlerPath))
+            {
+                string[] rIndexes = reqPath.Split('/');
+                string[] hIndexes = handlerPath.Split('/');
+
+                if (rIndexes.Length != hIndexes.Length)
+                    return false;
+
+                for (int i = 0; i < hIndexes.Length; i++)
+                {
+                    string rIndex = WebUtility.UrlDecode(rIndexes[i]);
+                    string hIndex = hIndexes[i];
+
+                    if (rIndex != hIndex && !new Regex(@"^:[^\/]+").IsMatch(hIndex))
+                        return false;
+                }
+
+                return true;
+            }
+            
+            return false;
+        }
+
+        private async Task HandleIncomingConnections()
+        {
+            while (true)
+            {
+                Exception error = null;
+                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse res = ctx.Response;
+
+                foreach (RouteHandler handler in handlers)
+                {
+                    if ((req.HttpMethod == handler.method) && PathMatched(req, handler.path))
+                    {
+                        try
+                        {
+                            bool next = false;
+                            req.Headers.Set("$SERVER_HANDLER_PATH", handler.path);
+                            handler.handler(req, res, () => next = true);
+                            if (!next)
+                            {
+                                if (!IsResponseResolved(res))
+                                    res.Close();
+                                break;
+                            }
+                        } catch (Exception e)
+                        {
+                            error = e;
+                            break;
+                        }
+                    }
+                }
+
+                // already resolved
+                if (IsResponseResolved(res)) continue;
+                
+                // has redirect
+                if (res.RedirectLocation != null)
+                {
+                    res.Close();
+                    continue;
+                }
+
+                // has error
+                if (error != null)
+                {
+                    res.StatusCode = 500;
+                    SendJson(new
+                    {
+                        message = error.Message,
+                        status = 500,
+                        data = error.StackTrace
+                    }, res);
+                    continue;
+                }
+
+                // default response
+                res.StatusCode = 404;
+                SendJson(new
+                {
+                    message = "Not Found",
+                    status = 404
+                }, res);
+            }
+        }
+
+        public void Post(string path, Action<HttpListenerRequest, HttpListenerResponse, Func<bool>>  handler)
+        {
+            RouteHandler _handler = new RouteHandler();
+            _handler.method = "POST";
+            _handler.path = path;
+            _handler.handler = handler;
+            handlers.Add(_handler);
+        }
+
+        public void Get(string path, Action<HttpListenerRequest, HttpListenerResponse, Func<bool>> handler)
+        {
+            RouteHandler _handler = new RouteHandler();
+            _handler.method = "GET";
+            _handler.path = path;
+            _handler.handler = handler;
+            handlers.Add(_handler);
+        }
+
+        public void Put(string path, Action<HttpListenerRequest, HttpListenerResponse, Func<bool>> handler)
+        {
+            RouteHandler _handler = new RouteHandler();
+            _handler.method = "PUT";
+            _handler.path = path;
+            _handler.handler = handler;
+            handlers.Add(_handler);
+        }
+
+        public void Delete(string path, Action<HttpListenerRequest, HttpListenerResponse, Func<bool>> handler)
+        {
+            RouteHandler _handler = new RouteHandler();
+            _handler.method = "DELETE";
+            _handler.path = path;
+            _handler.handler = handler;
+            handlers.Add(_handler);
+        }
+
+        public static void SendText(string text, HttpListenerResponse res)
+        {
+            res.ContentType = "text/html";
+            Send(Encoding.UTF8.GetBytes(text), res);
+        }
+
+        public static void SendJson(dynamic value, HttpListenerResponse res)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions(JsonSerializerDefaults.General);
+            options.WriteIndented = true;
+
+            string text = JsonSerializer.Serialize(value, options);
+            res.ContentType = "application/json";
+            Send(Encoding.UTF8.GetBytes(text), res);
+        }
+
+        public static Dictionary<string, string> GetParams(HttpListenerRequest req)
+        {
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            string reqPath = new Regex(@"\?.*").Replace(req.RawUrl, "");
+            string handlerPath = req.Headers.Get("$SERVER_HANDLER_PATH");
+            string[] rIndexes = reqPath.Split('/');
+            string[] hIndexes = handlerPath.Split('/');
+
+            if (rIndexes.Length == hIndexes.Length)
+            {
+                for (int i = 0; i < hIndexes.Length; i++)
+                {
+                    string rIndex = WebUtility.UrlDecode(rIndexes[i]);
+                    string hIndex = hIndexes[i];
+                    if (new Regex(@"^:[^\/]+").IsMatch(hIndex))
+                        param.Add(new Regex(@"^:").Replace(hIndex, ""), rIndex);
+                }
+            }
+            return param;
+        }
+
+        public static string GetBodyContent(HttpListenerRequest req)
+        {
+            Stream body = req.InputStream;
+            Encoding encoding = req.ContentEncoding;
+            StreamReader reader = new StreamReader(body, encoding);
+            string content = reader.ReadToEnd();
+            body.Close();
+            reader.Close();
+            return content;
+        }
+
+        public void Listen(int port)
+        {
+            string url = "http://127.0.0.1:" + port + "/";
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+            listener.Close();
+        } 
+    }
+}
---
+++ AuraController/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using AuraServiceLib;
+
+namespace AuraController
+{
+    struct RequestData
+    {
+        public List<uint> Lights { get; set; }
+        public uint Color { get; set; }
+    }
+
+    struct AuraDeviceLight
+    {
+        public uint Color { get; set; }
+        public string Name { get; set; }
+        public uint Red { get; set; }
+        public uint Green { get; set; }
+        public uint Blue { get; set; }
+    }
+
+    struct AuraDevice {
+        public uint Type { get; set; }
+        public string Name { get; set; }
+        public uint Height { get; set; }
+        public uint Width { get; set; }
+        public List<AuraDeviceLight> Lights { get; set; }
+    }
+
+    class Program
+    {
+        static bool active = false;
+        static IAuraSdk2 sdk = (IAuraSdk2)new AuraSdk();
+        static HttpServer server = new HttpServer();
+
+        static IAuraSyncDeviceCollection cache_devices = null; 
+
+        static void RequireActiveControl()
+        {
+            if (!active)
+                throw new Exception("Aura control is required to be active to proceeed with the action.");
+        }
+
+        static IAuraSyncDeviceCollection GetCachedDevices()
+        {
+            if (cache_devices == null) cache_devices = sdk.Enumerate(0);
+            return cache_devices;
+        }
+
+        static List<AuraDevice> GetDevices()
+        {   
+            IAuraSyncDeviceCollection devices = GetCachedDevices();
+
+            List<AuraDevice> _devices = new List<AuraDevice>();
+            foreach (IAuraSyncDevice device in devices)
+            {
+                AuraDevice _device = new AuraDevice()
+                {
+                    Type = device.Type,
+                    Name = device.Name,
+                    Height = device.Height,
+                    Width = device.Width,
+                    Lights = new List<AuraDeviceLight>(),
+                };
+
+                foreach (IAuraRgbLight light in device.Lights)
+                {
+                    _device.Lights.Add(new AuraDeviceLight()
+                    {
+                        Color = light.Color,
+                        Name = light.Name,
+                        Red = light.Red,
+                        Green = light.Green,
+                        Blue = light.Blue
+                    });
+                }
+                _devices.Add(_device);
+            }
+
+            return _devices;
+        }
+
+        static void Main(string[] args)
+        {
+            // returns if server is in control of aura
+            server.Get("/active", (req, res, next) =>
+            {  
+                HttpServer.SendJson(new { active }, res);
+            });
+
+            // activate control; acquire control to aura
+            server.Post("/activate", (req, res, next) =>
+            {
+                if (!active)
+                {
+                    sdk.SwitchMode();
+                    active = true;
+                }
+
+                HttpServer.SendJson(new {
+                    message = "activated successfully"
+                }, res);
+            });
+
+            // deactivate control; give up control to aura
+            server.Post("/deactivate", (req, res, next) =>
+            {
+                if (active)
+                {
+                    sdk.ReleaseControl(0);
+                    active = false;
+                }
+
+                HttpServer.SendJson(new
+                {
+                    message = "deactivated successfully"
+                }, res);
+            });
+            
+            // returns available devices
+            server.Get("/devices", (req, res, next) =>
+            {
+                RequireActiveControl();
+                HttpServer.SendJson(GetDevices(), res);
+            });
+
+            // set device lights color
+            server.Put("/devices/:device_name", (req, res, next) =>
+            {
+                RequireActiveControl();
+
+                Dictionary<string, string> param = HttpServer.GetParams(req);
+                string body = HttpServer.GetBodyContent(req);
+                RequestData config = JsonSerializer.Deserialize<RequestData>(body);
+                foreach (IAuraSyncDevice device in GetCachedDevices())
+                {
+                    if (device.Name == param["device_name"])
+                    {
+                        for (uint i = 0; i < device.Lights.Count; i++)
+                            if (config.Lights == null || config.Lights.Contains(i))
+                                device.Lights[(int)i].Color = config.Color;
+                        device.Apply();
+                        break;
+                    }
+                }
+
+                HttpServer.SendJson(GetDevices().Find(a => a.Name == param["device_name"]), res);
+            });
+
+            Console.WriteLine("Listening to port 8080");
+            server.Listen(8080);
+        }
+    }
+}
