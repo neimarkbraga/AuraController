8ea481258e37f76298079f8ab9a971643f397025
Makoy <neimarkbraga@gmail.com>
2022-02-16 15:32:52 +0800

    improved codes and changed port

--- AuraController/Program.cs
+++ AuraController/Program.cs
@@ -30,6 +30,7 @@ namespace AuraController
 
     class Program
     {
+        static int port = 54321;
         static bool active = false;
         static IAuraSdk2 sdk = (IAuraSdk2)new AuraSdk();
         static HttpServer server = new HttpServer();
@@ -81,75 +82,103 @@ namespace AuraController
             return _devices;
         }
 
+        static bool IsActive()
+        {
+            return active;
+        }
+
+        static void Activate()
+        {
+            if (!active)
+            {
+                sdk.SwitchMode();
+                active = true;
+            }
+        }
+
+        static void Deactivate()
+        {
+            if (active)
+            {
+                sdk.ReleaseControl(0);
+                active = false;
+            }
+        }
+
+        static void SetDeviceColor(string deviceName, RequestData config)
+        {
+            foreach (IAuraSyncDevice device in GetCachedDevices())
+            {
+                if (deviceName == null || deviceName == device.Name)
+                {
+                    for (uint i = 0; i < device.Lights.Count; i++)
+                        if (config.Lights == null || config.Lights.Contains(i))
+                            device.Lights[(int)i].Color = config.Color;
+                    device.Apply();
+                    break;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            // returns if server is in control of aura
-            server.Get("/active", (req, res, next) =>
-            {  
-                HttpServer.SendJson(new { active }, res);
-            });
-
-            // activate control; acquire control to aura
-            server.Post("/activate", (req, res, next) =>
+           try
             {
-                if (!active)
+                // initialize - acquire control by default
+                Activate();
+                SetDeviceColor(null, new RequestData()
                 {
-                    sdk.SwitchMode();
-                    active = true;
-                }
+                    Lights = null,
+                    Color = 0x0DE006B
+                });
 
-                HttpServer.SendJson(new {
-                    message = "activated successfully"
-                }, res);
-            });
+                // returns if server is in control of aura
+                server.Get("/active", (req, res, next) =>
+                {
+                    HttpServer.SendJson(new { active = IsActive() }, res);
+                });
 
-            // deactivate control; give up control to aura
-            server.Post("/deactivate", (req, res, next) =>
+                // activate control; acquire control to aura
+                server.Post("/activate", (req, res, next) =>
+                {
+                    Activate();
+                    HttpServer.SendJson(new { Message = "activated successfully" }, res);
+                });
+
+                // deactivate control; give up control to aura
+                server.Post("/deactivate", (req, res, next) =>
+                {
+                    Deactivate();
+                    HttpServer.SendJson(new { Message = "deactivated successfully" }, res);
+                });
+
+                // returns available devices
+                server.Get("/devices", (req, res, next) =>
+                {
+                    RequireActiveControl();
+                    HttpServer.SendJson(GetDevices(), res);
+                });
+
+                // set device lights color
+                server.Put("/devices/:device_name", (req, res, next) =>
+                {
+                    RequireActiveControl();
+
+                    Dictionary<string, string> param = HttpServer.GetParams(req);
+                    string body = HttpServer.GetBodyContent(req);
+                    RequestData config = JsonSerializer.Deserialize<RequestData>(body);
+                    SetDeviceColor(param["device_name"], config);
+                    HttpServer.SendJson(GetDevices().Find(a => a.Name == param["device_name"]), res);
+                });
+
+                Console.WriteLine("Server started at port " + port);
+                server.Listen(port);
+            }
+            catch (Exception e)
             {
-                if (active)
-                {
-                    sdk.ReleaseControl(0);
-                    active = false;
-                }
-
-                HttpServer.SendJson(new
-                {
-                    message = "deactivated successfully"
-                }, res);
-            });
-            
-            // returns available devices
-            server.Get("/devices", (req, res, next) =>
-            {
-                RequireActiveControl();
-                HttpServer.SendJson(GetDevices(), res);
-            });
-
-            // set device lights color
-            server.Put("/devices/:device_name", (req, res, next) =>
-            {
-                RequireActiveControl();
-
-                Dictionary<string, string> param = HttpServer.GetParams(req);
-                string body = HttpServer.GetBodyContent(req);
-                RequestData config = JsonSerializer.Deserialize<RequestData>(body);
-                foreach (IAuraSyncDevice device in GetCachedDevices())
-                {
-                    if (device.Name == param["device_name"])
-                    {
-                        for (uint i = 0; i < device.Lights.Count; i++)
-                            if (config.Lights == null || config.Lights.Contains(i))
-                                device.Lights[(int)i].Color = config.Color;
-                        device.Apply();
-                        break;
-                    }
-                }
-
-                HttpServer.SendJson(GetDevices().Find(a => a.Name == param["device_name"]), res);
-            });
-
-            Console.WriteLine("Listening to port 8080");
-            server.Listen(8080);
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
     }
 }
